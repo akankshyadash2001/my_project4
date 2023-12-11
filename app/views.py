33b287143565da0caf8af749da2b497cb295d2d1
akankshyadash2001 <akankshyadash2001.com>
2023-12-11 12:10:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+def akankshya(request):
+    return render(request,'akankshya.html')
+
+
+
+def limca(request):
+    return render(request,'limca.html')
+# Create your views here.
